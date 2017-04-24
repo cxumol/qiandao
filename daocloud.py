dfecd44c8749df39e3dac67ed5812290359ca44a
KaiyangNi <KaiyangNi@users.noreply.github.com>
2017-04-24 10:20:33 +0800

    Update daocloud.py

--- daocloud.py
+++ daocloud.py
@@ -1,1 +1,20 @@
-q
+# coding: utf-8
+
+import requests
+
+# if you don't have a config.py
+# You can set v2ex_username, v2ex_password yourself
+
+appid = os.getenv('appid')
+token = os.getenv('token')
+
+
+def daocloud_restart():
+    headers = {"Authorization": token}
+    session = requests.Session()
+    session.headers.update(headers)
+    
+    resp = session.post("https://openapi.daocloud.io/v1/apps/"+ appid +"/actions/restart", {})
+    
+if __name__ == '__main__':
+    daocloud_restart()
